5dc7e243b9d405ed4d919cce5fc52614e1800745
Pakan Talebi <pakantalebi2009@gmail.com>
2023-11-05 22:51:12 +0330

    changing the project

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask,request,Response
-import os
-import base64
+from flask import Flask, request, Response
 import pandas as pd
+from io import BytesIO
+import base64
 
 app = Flask(__name__,static_folder="static")
 
@@ -15,14 +15,7 @@ def Home():
 @app.route("/",methods=["POST"])
 def read_Excel():
     body = request.get_json()
-    file_base64 = body["FILE"]
-    Name = body["Username"]
-    destition_file = open(os.path.join("F:\\Mr.Rozbeh_project\\MeMber's File",f"{Name}.xlsx") , "wb+")
-    base64_bytes = file_base64.encode('ascii')
-    file_bytes = base64.b64decode(base64_bytes)
-    newFileByteArray = bytearray(file_bytes)
-    destition_file.write(newFileByteArray)
-    Excel_data = pd.read_excel(f"F:\\Mr.Rozbeh_project\\MeMber's File\\{Name}.xlsx",sheet_name="Sheet1")
+    Excel_data = pd.read_excel(BytesIO(base64.b64decode(body["Bytes"])))
     Average = 0
     for lesson in range(len(Excel_data["نمرات"].tolist())):
         Average += Excel_data["نمرات"].tolist()[lesson]
@@ -42,4 +35,5 @@ def stream():
         yield 'data: {}___{}___{}___{}\n\n'.format(list_Average[0],list_Lessons,max(list_Average[1:]),min(list_Average[1:]))
     return Response(eventStream(), mimetype="text/event-stream")
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
